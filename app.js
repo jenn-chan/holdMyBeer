cf41dca564a126d144eef1fc6c624be6bbd0c114
jsc058 <jsc058@ucsd.edu>
2020-05-18 18:53:16 -0700

    add comments explaining almost everything

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ const express = require("express"); // web server framework we're using
 const app = express();
 const bodyParser = require("body-parser"); // to parse info from a post request
 const mongoose = require("mongoose");
+const passport = require("passport");   // authentication middleware for node.js with different strategies (login, fb, twitter, etc.)
+const LocalStrategy = require("passport-local"); // local strategy = username and passpord
 const Beer = require("./models/beer");
+const User = require("./models/user");
 const Comment = require("./models/comment");
+
 //var seedDB = require("./seeds");
 
 //seedDB();
@@ -16,6 +20,23 @@ app.use(bodyParser.json()); // parse JSON request body to be able to call res.js
 app.use(bodyParser.urlencoded({extended: true})); // parsing bodies from url (key-val pairs)
 app.set("view engine", "ejs");
 
+// Passport Configuration
+app.use(require("express-session")({    // need to include seperate package for session and express
+    // !!! in production => produce a randomly generated string from environment var
+    secret: "HoldMyBeer",   // used to sign the session ID cookie (salt for the hash/ encode and decode the sessions)
+    // forces the session to be saved back to the session store (even if it was never modified during request)
+    resave: false,
+    // saveUnitialized - forces a session that is unitialized (new but not modified) to be saved to the store
+    saveUninitialized: false // false - reduces server storage usage and comply with laws that require permission before setting a cookie
+}));
+
+app.use(passport.initialize()); // middleware that's required to initialize Passport
+app.use(passport.session()); // session - provide state to http requests (sending along user info with request)
+passport.use(new LocalStrategy(User.authenticate())); // using locaal strategy
+
+// tell passport to use what's already defined on the user (methods we got from plugging in passportLocalMongoose)
+passport.serializeUser(User.serializeUser()); // setting id as cookie in user's browser (encoding)
+passport.deserializeUser(User.deserializeUser()); // getting id from cookie => used in callback to get user info (decoding)
 
 app.get("/", function(req, res) {
     res.redirect("/beers"); 
